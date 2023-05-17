2dae221857783a92a8afcc15c2f53443e59f126d
Isa Rethans <isa.rethans@9to5.software>
2023-05-17 16:14:56 +0100

    add prometeus

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,7 @@
 printf "Deploying Kubernetes components...\n"
 kubectl apply -f model-service.yml
 kubectl apply -f webapp.yml
+kubectl apply -f monitoring.yml
 kubectl apply -f ingress.yml
 
 minikube service list
