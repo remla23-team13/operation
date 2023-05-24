2d6ca6ddc7f29a44b60b995a9087cb89e95a3994
Isa Rethans <isa.rethans@9to5.software>
2023-05-24 09:40:55 +0200

    change kubernetes set up

--- delete_kub.sh
+++ delete_kub.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-kubectl delete -f model-service.yml
-kubectl delete -f webapp.yml
-kubectl delete -f monitoring.yml
-kubectl delete -f ingress.yml
-
-minikube stop
+kubectl delete -f kubernetes/model-service.yml
+kubectl delete -f kubernetes/webapp.yml
+kubectl delete -f kubernetes/ingress.yml
