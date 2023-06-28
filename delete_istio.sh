d4bff2a43cb25e5a02d129f37cdf29f505ec5ab0
Sara Op den Orth <sara@opdenorth.com>
2023-06-28 10:16:18 +0200

    Fix label removal kubectl

--- delete_istio.sh
+++ delete_istio.sh
@@ -4,7 +4,9 @@ kubectl delete -f istio/
 printf "\n--------\nDeleting all istio addons\n--------\n"
 kubectl delete -f istio/addons/
 printf "\n--------\nDisabling istio-injection\n--------\n"
-kubectl label ns default istio-injection=disabled
+kubectl label ns default istio-injection=disabled --overwrite
+kubectl label ns default istio-injection-
+kubectl get ns default --show-labels
 printf "\n--------\nUninstalling istioctl from cluster\n--------\n"
 istioctl uninstall --purge
 
